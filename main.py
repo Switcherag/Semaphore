38b343b111d10f63358c7b7096ebfd485d64d7ac
Your Name <you@example.com>
2023-10-13 10:53:06 +0200

    ff

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from semaphore import Semaphore
 import time
 import matplotlib.pyplot as plt
 import numpy as np
-import _thread
 
 
 def sampling(Semaphore,bufferSize = 256, sampling_period = 0.0016):
@@ -18,7 +17,8 @@ def sampling(Semaphore,bufferSize = 256, sampling_period = 0.0016):
         
     Semaphore.Proberen(filtering,Semaphore, Y)
     Semaphore.Proberen(sampling,Semaphore, bufferSize, sampling_period)
-    
+   
+
     return None
 
 def filtering(Semaphore, buffer):
@@ -27,15 +27,16 @@ def filtering(Semaphore, buffer):
         if buffer[i] < 0:
             buffer[i] = 0
     
-    Semaphore.Proberen(display_update,Semaphore,  buffer)
+    Semaphore.Proberen(display_update_filtering,Semaphore,  buffer)
     
     return None
 
       
     
-def display_update(Semaphore, buffer):
+def display_update_filtering(Semaphore, buffer):
     
-    plt.title("Thread count : " + str(_thread._count()) +" | Semaphore count : " + str(Semaphore.value) + " | Main thread active: " + str(active))
+    plt.title("Thread mean : " + str(round(Semaphore.thread_sum/Semaphore.pass_count,3)) +" | Semaphore mean count : " + str(round(Semaphore.value_sum/Semaphore.pass_count,3)) + " | Main thread active: " + str(active))
+
     global data
     #shift the data by the current buffer
     data = np.roll(data, -len(buffer))
@@ -44,12 +45,13 @@ def display_update(Semaphore, buffer):
     line.set_ydata(data)
 
     plt.draw()
+
          
 if __name__ == "__main__":
 
     MySemaphore = Semaphore(3)
     active = True
-    MyBufferSize = 64
+    MyBufferSize = 128
     MySamplingPeriod = 0.0016
     MyFrameNumber = 10
     display_init = False
@@ -59,7 +61,9 @@ if __name__ == "__main__":
     SamplesNumber = MyBufferSize * MyFrameNumber
     x = np.linspace(0, maxT, SamplesNumber)
     data = np.zeros(SamplesNumber)
+    
     line, = plt.plot(x, data)
+    
     plt.ylim(-1.5, 1.5)
     plt.show()
 
--- semaphore.py
+++ semaphore.py
@@ -1,30 +1,40 @@
 #class Semaphore
 import _thread
-import time
 
 class Semaphore:
     def __init__(self, initial):
         self.lock = _thread.allocate_lock()
         self.value = initial
         self.queue = []
+        self.value_sum = 0
+        self.thread_sum = 0
+        self.pass_count = 0
 
     def Proberen(self, process, *args):
         self.lock.acquire()
+        self.value_sum += self.value
+        self.thread_sum += _thread._count()
+        self.pass_count += 1
+
         if (self.value <= 0):
             self.lock.release()
             self.queue.append((process, *args))
             if len(self.queue) > 20:
                 print("Le programme court derrière")
-        else:
+        else:            
             self.value -= 1
             self.lock.release()  
             _thread.start_new_thread(self._wrapper, (process, args))           
 
     def Verhogen(self):
         self.lock.acquire()
+
+        self.value_sum += self.value
+        self.thread_sum += _thread._count()
+        self.pass_count += 1
+        
         if (len(self.queue) > 0):
             process, *args = self.queue.pop(0)
-            self.value -= 1
             self.lock.release()
             _thread.start_new_thread(self._wrapper, (process, args))
         else:       
